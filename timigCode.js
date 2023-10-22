97f0e523e213b00edf51bc4eb97bc822ad8ba128
root <root@Im-Laptop.localdomain>
2023-10-22 11:48:04 -0400

    add up to n fn two different way

--- timigCode.js
+++ timigCode.js
@@ -1,1 +1,16 @@
 console.log(`hello world`)
+
+let total = 0
+function addUpTo(n) {
+    for (let i=0; i <= n; i++){
+        total += i
+    }
+    return total
+}
+
+const addUpToShort = (n) => {
+    return  n * (n+1) / 2;
+}
+
+console.log(addUpTo(100))
+console.log(addUpToShort(100))
