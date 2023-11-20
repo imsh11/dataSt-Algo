3e191699825bce8d681e60084df34d7de324a844
Imran <imransh11@gmail.com>
2023-11-19 20:28:56 -0500

    comments added

--- timigCode.js
+++ timigCode.js
@@ -1,5 +1,10 @@
 console.log(`hello world`)
 
+//better code
+// - faster?
+// - less memory
+// - - more readable
+
 let total = 0
 function addUpTo(n) {
     for (let i=0; i <= n; i++){
@@ -9,8 +14,17 @@ function addUpTo(n) {
 }
 
 const addUpToShort = (n) => {
+    // zomg wut algorithem
     return  n * (n+1) / 2;
 }
 
-console.log(addUpTo(100))
-console.log(addUpToShort(100))
+// let t1 = performance.now();
+// console.log(addUpTo(100000000))
+// let t2 = performance.now()
+
+//shorter
+let t1 = performance.now()
+console.log(addUpToShort(100000000))
+let t2 = performance.now()
+
+console.log(`Time elasped: ${t2 - t1} miliseconds`)
