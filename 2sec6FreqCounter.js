67b68c25b905a75e01c71a720d6ec42b05858bb9
Imran <imransh11@gmail.com>
2023-11-20 10:49:46 -0500

    checking in one array

---
+++ 2sec6FreqCounter.js
@@ -0,0 +1,10 @@
+const same = (arr1, arr2) => {
+
+    for(let i=0; i<arr1.length; i++){
+        if(!arr2.includes(arr1[i] ** 2)) return false
+    }
+    return true
+}
+
+console.log(same([1,2,3,4], [4,1,9]))
+// console.log(same())
