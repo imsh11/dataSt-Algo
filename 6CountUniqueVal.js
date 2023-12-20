c05ade3a0304e09ad463dbeaad4e5c916803714d
Imran <imransh11@gmail.com>
2023-12-20 17:48:16 -0500

    saving changes

--- 6CountUniqueVal.js
+++ 6CountUniqueVal.js
@@ -21,8 +21,8 @@ let countUnique = (x) => {
 
         // value = x[0] // would reassign every loop
 
-        // if (!value){ // 
-            // value = x[i] // value stuck at 1
+        // if (!value ){ // if 0 present problem
+        //     value = x[i] // value stuck at 1
         // }
         console.log(val, value, 'values')
         if (val !== value){
@@ -37,6 +37,6 @@ let countUnique = (x) => {
 }
 
 // console.log(countUnique([1,1,1,1,1,2])) // 2
-console.log(countUnique([1,2,3,4,4,4,7,7,12,12,13])) // 7
+// console.log(countUnique([1,2,3,4,4,4,7,7,12,12,13])) // 7
 // console.log(countUnique([])) // 0
-// console.log(countUnique([-2,-1,-1,0,1])) // 4
+console.log(countUnique([-2,-1,-1,0,1])) // 4
